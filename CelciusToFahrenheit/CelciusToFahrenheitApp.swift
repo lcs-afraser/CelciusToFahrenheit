c68b86abd35d44622282fdc8e2ad41d534c082e0
Alistair Fraser <afraser@lcs.on.ca>
2023-01-17 12:23:03 -0500

    Initial Commit

---
+++ CelciusToFahrenheit/CelciusToFahrenheitApp.swift
@@ -0,0 +1,17 @@
+//
+//  CelciusToFahrenheitApp.swift
+//  CelciusToFahrenheit
+//
+//  Created by Alistair Fraser on 2023-01-17.
+//
+
+import SwiftUI
+
+@main
+struct CelciusToFahrenheitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
